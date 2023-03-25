2c2a77d67722747983ef20665120914bd0633856
JasonPC <jasonwong287962@gmail.com>
2023-03-25 01:37:06 -0500

    made a controller base

---
+++ controller/inventoryController.js
@@ -0,0 +1,13 @@
+const Category = require('../models/Category');
+const Items = require('../models/Items');
+// for forms
+const {body, validationResult} = require('express-validator');
+
+exports.category_list = (req,res) => {
+
+}
+
+exports.items_list = (req, res) => {
+
+  
+}
