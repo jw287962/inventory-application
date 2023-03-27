c29ec4c32fea079ce512d37720eb1a849aeda797
JasonPC <jasonwong287962@gmail.com>
2023-03-27 01:58:35 -0500

    implemented basic view for indivudal item

--- controller/inventoryController.js
+++ controller/inventoryController.js
@@ -7,7 +7,30 @@ exports.category_list = (req,res) => {
 
 }
 
-exports.items_list = (req, res) => {
+exports.view_item = (req,res) => {
+    res.send('set up view item')
+}
 
-  
+exports.items_list = async (req, res) => {
+  const result = await Items.find()  
+    .sort({category: 1})
+    .populate("category")
+
+   
+  res.render('item_list', {
+          title: 'Items List',
+          items: result,
+        })
+    // .exec( function (err, list_items){
+    //     if(err) {
+    //       return next(err);
+    //     }
+
+    //     res.render('item_list', {
+    //       title: 'Items List',
+    //       items: list_items,
+    //     })
+        
+    // })
+
 }
--- routes/items.js
+++ routes/items.js
@@ -3,9 +3,12 @@ var router = express.Router();
 const inventoryController = require('../controller/inventoryController')
 
 /* GET /items listing. */
-router.get('/', inventoryController.items_list);
+router.get('/items', inventoryController.items_list);
+router.get('/', () => {
+  res.redirect('/category/items')
+});
 
-
+router.get('/item/:id', inventoryController.view_item);
 
 // Category
 router.get('/category', inventoryController.category_list);
